d1122dce10967d4e14346da8028f7c8744ace61a
Loic Sharma <sharma.loic@gmail.com>
2021-08-29 16:29:05 -0700

    Load search results

--- src/BaGet.Web/Pages/Index.cshtml.cs
+++ src/BaGet.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BaGet.Core;
+using BaGet.Protocol.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BaGet.Web
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ISearchService _search;
 
+        public IndexModel(ISearchService search)
+        {
+            _search = search ?? throw new ArgumentNullException(nameof(search));
+        }
+
+        public SearchResponse SearchResults { get; private set; }
+
+        public async Task OnGetAsync(CancellationToken cancellationToken)
+        {
+            SearchResults = await _search.SearchAsync(
+                new SearchRequest
+                {
+                    Take = 20,
+                    IncludeSemVer2 = true,
+                },
+                cancellationToken);
         }
     }
 }
